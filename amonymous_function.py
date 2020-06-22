250236191b425626c90d6159e7f54bab420b878f
Harsh Kailash <harshkailash2000@gmail.com>
2020-06-22 22:21:35 +0530

    Create amonymous_function.py

---
+++ amonymous_function.py
@@ -0,0 +1,15 @@
+f= lambda a : a*a
+
+a=int(input("enter a number"))
+result=f(a)
+
+print (result)
+
+
+g= lambda x,y : x+y
+
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+result1=g(x,y)
+
+print (result1)

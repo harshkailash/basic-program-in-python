de844d4edcf7f763c61b86c2c8994766a5186577
Harsh Kailash <harshkailash2000@gmail.com>
2020-06-23 23:18:56 +0530

    Add files via upload

---
+++ own_iterator.py
@@ -0,0 +1,17 @@
+class ten:
+    def __init__(self):
+        self.num=1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.num<=10:
+            val=self.num
+            self.num+=1
+            return val
+        else:
+            raise StopIteration
+
+values=ten()
+
+for i in values:
+    print(i)
